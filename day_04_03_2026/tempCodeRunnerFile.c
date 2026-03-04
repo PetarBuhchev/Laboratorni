83c7c0943060f932ea279b68f569174dd5553c30
PetarBuhchev <petarbuhchev@outlook.com>
2026-03-04 08:27:44 +0200

    Add actual C solutions

--- day_04_03_2026/tempCodeRunnerFile.c
+++ day_04_03_2026/tempCodeRunnerFile.c
@@ -1,36 +1,20 @@
-//Да се прочете едно число от 1 до 7 и да се изведе на екрана кой ден от седмицата съответства на това число. Да се използва switch оператор.
+//Да се напише програма която отпечатва числата от 1 до 10 с while цикъл и с for цикъл.
 #include <stdio.h>
 
 int main() {
-    int day;
-    printf("Enter a number (1-7) to find out the corresponding day of the week: ");
-    scanf("%d", &day);
-
-    switch (day) {
-        case 1:
-            printf("Monday\n");
-            break;
-        case 2:
-            printf("Tuesday\n");
-            break;
-        case 3:
-            printf("Wednesday\n");
-            break;
-        case 4:
-            printf("Thursday\n");
-            break;
-        case 5:
-            printf("Friday\n");
-            break;
-        case 6:
-            printf("Saturday\n");
-            break;
-        case 7:
-            printf("Sunday\n");
-            break;
-        default:
-            printf("Invalid input. Please enter a number between 1 and 7.\n");
+    int i = 1;
+    printf("Using while loop:\n");
+    while (i <= 10) {
+        printf("%d ", i);
+        i++;
     }
+    printf("\n");
+
+    printf("Using for loop:\n");
+    for (i = 1; i <= 10; i++) {
+        printf("%d ", i);
+    }
+    printf("\n");
 
     return 0;
 }

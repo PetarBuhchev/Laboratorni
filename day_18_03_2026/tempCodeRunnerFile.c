9ff719da2de965da19b46be7e85693390de04c15
PetarBuhchev <petarbuhchev@outlook.com>
2026-03-18 09:39:56 +0200

    task12.c

--- day_18_03_2026/tempCodeRunnerFile.c
+++ day_18_03_2026/tempCodeRunnerFile.c
@@ -1,13 +1,23 @@
-//Да се напише функция, която намира дължината на низ, без да използва вградените функции за работа с низове.
+//Напишете програма, която намира броя на думите в низ. Низът е на един ред и думите са разделени с интервал. Броят на думите се извежда на конзолата.
 #include <stdio.h>
 
-int main()
-{
-    char str[] = "Hello, World!";
-    int length = 0;
+int main() {
+    char str[100];
+    int wordCount = 0;
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
     for (int i = 0; str[i] != '\0'; i++) {
-        length++;
+        if (str[i] == ' ') {
+            wordCount++;
+        }
     }
-    printf("The length of the string is: %d\n", length);
+
+    // Ако последният символ не е интервал или нов ред, добавяме 1 към броя на думите
+    if (str[0] != ' ') {
+        wordCount++;
+    }
+
+    printf("The number of words in the string is: %d\n", wordCount);
     return 0;
 }

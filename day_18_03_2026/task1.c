4def0c6411d83aa3fb155a4d23b1fe0bc96dc113
PetarBuhchev <petarbuhchev@outlook.com>
2026-03-18 07:55:56 +0200

    Tasks from today

---
+++ day_18_03_2026/task1.c
@@ -0,0 +1,17 @@
+//Да се създаде масив с 10 елемента. Да се впишат числата от 1 до 10 в масива и да се отпечатат на екрана.
+#include <stdio.h>
+
+int main() {
+    int arr[10];
+    
+    for (int i = 0; i < 10; i++) {
+        arr[i] = i + 1;
+    }
+    
+    printf("The numbers from 1 to 10 are:\n");
+    for (int i = 0; i < 10; i++) {
+        printf("%d\n", arr[i]);
+    }
+    
+    return 0;
+}

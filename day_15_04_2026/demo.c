e430106ea4f30d9ef6e8046c61a3b8d49480d7cb
PetarBuhchev <petarbuhchev@outlook.com>
2026-04-15 08:06:55 +0300

    fix demo.c

--- day_15_04_2026/demo.c
+++ day_15_04_2026/demo.c
@@ -2,7 +2,7 @@ struct Books {
     char title[40];
     char author[20];
     char theme[30];
-    int book_id;
+    int id_book;
 } ;
 
 int main() {
@@ -13,20 +13,20 @@ int main() {
     strcpy(book1.title, "The Great Gatsby");
     strcpy(book1.author, "F. Scott Fitzgerald");
     strcpy(book1.theme, "Classic Literature");
-    book1.book_id = 1;
+    book1.id_book = 1;
 
     // Assigning values to book2
     strcpy(book2.title, "To Kill a Mockingbird");
     strcpy(book2.author, "Harper Lee");
     strcpy(book2.theme, "Classic Literature");
-    book2.book_id = 2;
+    book2.id_book = 2;
 
     // Printing book details
     printf("Book 1:\nTitle: %s\nAuthor: %s\nTheme: %s\nBook ID: %d\n\n",
-           book1.title, book1.author, book1.theme, book1.book_id);
+           book1.title, book1.author, book1.theme, book1.id_book);
 
     printf("Book 2:\nTitle: %s\nAuthor: %s\nTheme: %s\nBook ID: %d\n",
-           book2.title, book2.author, book2.theme, book2.book_id);
+           book2.title, book2.author, book2.theme, book2.id_book);
 
     return 0;
 }
